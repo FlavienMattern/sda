a8a00a16544eb7c584f9b082fff8bfce93eb4180
FlavienMattern <f.mattern@unistra.fr>
2025-07-24 17:08:59 +0200

    Show results if database is loaded

--- sda/streamlit/pages/station_map.py
+++ sda/streamlit/pages/station_map.py
@@ -6,29 +6,29 @@ st.title(":material/map: Map Explorer")
 st.divider()
 database.status()
 
+if database.is_loaded():
+    ########### Prepare Data ###########
 
-########### Prepare Data ###########
+    import pickle as pkl
+    import pandas as pd
 
-import pickle as pkl
-import pandas as pd
+    inventory_file = "/media/flavien/WORK/these/tools/inventory_alsace.pkl"
+                    
+    with open(inventory_file, "rb") as f:
+        inventory = pkl.load(f)
 
-inventory_file = "/media/flavien/WORK/these/tools/inventory_alsace.pkl"
-                
-with open(inventory_file, "rb") as f:
-    inventory = pkl.load(f)
-
-inventory = pd.DataFrame(inventory)
-inventory = inventory.drop(columns=["geometry"])
-inventory['Channels'] = inventory['Channels'].apply(lambda x: list(dict.fromkeys(x)))
+    inventory = pd.DataFrame(inventory)
+    inventory = inventory.drop(columns=["geometry"])
+    inventory['Channels'] = inventory['Channels'].apply(lambda x: list(dict.fromkeys(x)))
 
 
 
-########### Configure Layout ###########
+    ########### Configure Layout ###########
 
-row = st.columns([0.5, 0.5])
+    row = st.columns([0.5, 0.5])
 
-tile = row[0].container()
-modules.map_stations(tile, inventory)
+    tile = row[0].container()
+    modules.map_stations(tile, inventory)
 
-tile = row[1].container()
-modules.dataframe_stations(tile, inventory)
+    tile = row[1].container()
+    modules.dataframe_stations(tile, inventory)
