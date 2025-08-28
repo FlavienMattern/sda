48855af238430ff5e8de08ec5e15852e9373c0cf
FlavienMattern <f.mattern@unistra.fr>
2025-08-28 15:42:08 +0200

    Add condition on page name

--- sda/streamlit/functions/custom_pages.py
+++ sda/streamlit/functions/custom_pages.py
@@ -56,41 +56,45 @@ def create(name):
     if "custom_pages" not in st.session_state.get("session")["content"].keys():
         st.session_state["session"]["content"]["custom_pages"] = {}
 
+    if name in ["", None] or name.isspace():
+        popup_error(f"You need to enter a valid name !")
+        return
+
     if name in st.session_state.get("session")["content"]["custom_pages"].keys():
         popup_error(f"A custom page with the name **{name}** already exists !")
+        return
 
+    pages = get_pages()
+    wdir = st.session_state.get("database")["settings"]["wdir"]
+    session_id =  st.session_state.get("session")["settings"]["id"]
+    
+    if len(pages) == 0:
+        page_id = 0
     else:
-        pages = get_pages()
-        wdir = st.session_state.get("database")["settings"]["wdir"]
-        session_id =  st.session_state.get("session")["settings"]["id"]
-        
-        if len(pages) == 0:
-            page_id = 0
-        else:
-            page_id = max(list(pages.index)) + 1
+        page_id = max(list(pages.index)) + 1
 
-        # Update list of pages
-        pages_folder = os.path.join(wdir, "streamlit", f"session_{session_id:03d}", "custom_pages")
-        pages_filename = os.path.join(pages_folder, "pages.txt")
-        pages.loc[page_id] = [name, f"page_{page_id:03d}"]
-        
-        os.makedirs(pages_folder, exist_ok=True)
+    # Update list of pages
+    pages_folder = os.path.join(wdir, "streamlit", f"session_{session_id:03d}", "custom_pages")
+    pages_filename = os.path.join(pages_folder, "pages.txt")
+    pages.loc[page_id] = [name, f"page_{page_id:03d}"]
+    
+    os.makedirs(pages_folder, exist_ok=True)
 
-        pages.to_csv(pages_filename)
+    pages.to_csv(pages_filename)
 
-        # Create page layout
-        custom_page_folder = os.path.join(wdir, "streamlit", f"session_{session_id:03d}", "custom_pages", f"page_{page_id:03d}")
-        custom_page_filename = os.path.join(custom_page_folder, f"layout_{page_id:03d}.py")
+    # Create page layout
+    custom_page_folder = os.path.join(wdir, "streamlit", f"session_{session_id:03d}", "custom_pages", f"page_{page_id:03d}")
+    custom_page_filename = os.path.join(custom_page_folder, f"layout_{page_id:03d}.py")
 
-        if os.path.exists(custom_page_folder):
-            shutil.rmtree(custom_page_folder)
-        
-        os.makedirs(custom_page_folder, exist_ok=True)
-        
-        with open(custom_page_filename, "w") as f:
-            f.write(layout(name))
+    if os.path.exists(custom_page_folder):
+        shutil.rmtree(custom_page_folder)
+    
+    os.makedirs(custom_page_folder, exist_ok=True)
+    
+    with open(custom_page_filename, "w") as f:
+        f.write(layout(name))
 
-        st.rerun()
+    st.rerun()
 
 
 
@@ -145,3 +149,7 @@ def remove_check(id):
     st.warning(f":material/warning: You are about to permanently remove the custom page : **{page_name}**. You will loose all information on this page. Close this popup if it was a mistake.")
     if st.button(":material/check: Remove Page"):
         remove(id)
+
+def visibility(id):
+    pages = get_pages()
+    page_name = pages[pages.index == id].page_name.values[0]
