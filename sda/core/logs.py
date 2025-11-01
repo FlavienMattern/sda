3ecb4df067795d62d4b7334649b305219ba0c6c8
FlavienMattern <f.mattern@unistra.fr>
2025-11-01 16:23:06 +0100

    Improve logging

--- sda/core/logs.py
+++ sda/core/logs.py
@@ -4,6 +4,7 @@ import multiprocessing as mp
 import os
 import sys
 from datetime import datetime
+import __main__
 
 
 # Global variables
@@ -19,11 +20,14 @@ def _get_main_script_name():
     return os.path.splitext(main_script)[0] or "main"
 
 
-def _listener_configurer(log_dir):
+def _listener_configurer(log_dir, log_name):
     # Logfile initiation
     os.makedirs(log_dir, exist_ok=True)
-    log_name = _get_main_script_name()
-    log_file = os.path.join(log_dir, f"{log_name}_{datetime.now():%Y-%m-%d_%H%M%S}.log")
+    if log_name is None:
+        log_name = _get_main_script_name()
+        log_file = os.path.join(log_dir, f"{log_name}_{datetime.now():%Y-%m-%d_%H%M%S}.log")
+    else:
+        log_file = os.path.join(log_dir, log_name)
 
     msg = """
     ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓                                       
@@ -46,12 +50,13 @@ def _listener_configurer(log_dir):
     root.addHandler(handler)
     root.setLevel(_log_level)
     
-    logging.info(f"Started logging to {log_file}")
+    print(f"{datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} Start logging to: {log_file}")
+    logging.info(f"Start logging.")
 
 
-def _listener_process_main(queue, log_dir):
+def _listener_process_main(queue, log_dir, log_name):
     # Logs recovery and writing
-    _listener_configurer(log_dir)
+    _listener_configurer(log_dir, log_name)
     while True:
         record = queue.get()
         if record is None:
@@ -69,7 +74,7 @@ def _worker_configurer():
     root.setLevel(_log_level)
 
 
-def start_logging(log_dir="logs", level="INFO"):
+def start_logging(log_dir=None, log_name=None, level="INFO"):
     """
     Initiate logging.
     
@@ -82,26 +87,41 @@ def start_logging(log_dir="logs", level="INFO"):
     if _log_initialized:
         return
 
+    if log_dir is None:
+        wdir = os.path.dirname(os.path.abspath(__main__.__file__))
+        log_dir = os.path.join(wdir, "logs")
+        os.makedirs(log_dir, exist_ok=True)
+
     level = level.upper()
     if not hasattr(logging, level):
-        raise ValueError(f"Wrong logging level : {level}")
+        raise ValueError(f"Wrong logging level: {level}")
     _log_level = getattr(logging, level)
 
     _log_queue = mp.Queue()
     _listener_process = mp.Process(
-        target=_listener_process_main, args=(_log_queue, log_dir), daemon=True
+        target=_listener_process_main, args=(_log_queue, log_dir, log_name), daemon=True
     )
     _listener_process.start()
+
+    # configure once for this process
+    _worker_configurer()
+
     _log_initialized = True
+    add_log(f"File executed: {os.path.abspath(__main__.__file__)}", level="info")
 
 
 def stop_logging():
+   
+    add_log("Stop logging.", level="info")
+    print(f"{datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} Stop logging.")
+    
     global _log_queue, _listener_process, _log_initialized
     if not _log_initialized:
         return
     _log_queue.put(None)
     _listener_process.join(timeout=2)
     _log_initialized = False
+    
 
 
 def add_log(message, level="info"):
@@ -119,7 +139,6 @@ def add_log(message, level="info"):
         start_logging()
 
     logger = logging.getLogger("sda")
-    _worker_configurer()
 
     log_fn = getattr(logger, level.lower(), logger.info)
     log_fn(message)
