79ddada6129a17d2a8360c7e7a8d55ef93f0613c
FlavienMattern <f.mattern@unistra.fr>
2025-11-01 22:33:39 +0100

    Automatically define wrappers and modules from folder names

--- sda/process/__init__.py
+++ sda/process/__init__.py
@@ -1,6 +1,14 @@
-from .xcorr_noise import xcorr_noise
-from .xcorr_noise_postprocessing import xcorr_noise_postprocessing
-from .scan_data import scan_data
-from .ppsd import ppsd
+from pathlib import Path
 
-__all__ = ["scan_data", "xcorr_noise", "xcorr_noise_postprocessing", "ppsd"]
+# Directory of the __init__.py file
+_current_dir = Path(__file__).parent
+__all__ = []
+
+# Dynamically import all modules in the current directory
+for _item in sorted(_current_dir.iterdir()):
+    if _item.is_dir() and not _item.name.startswith("_"):
+        _module_name = _item.name
+        exec(f"from .{_module_name} import {_module_name}")
+        __all__.append(_module_name)
+
+del _current_dir, _item, _module_name, Path
--- sda/process/ppsd/__init__.py
+++ sda/process/ppsd/__init__.py
@@ -1,6 +1,20 @@
-from .main import ppsd as main
+# This __init__.py file imports the main function from main.py directly via a wrapper
+from pathlib import Path
+import importlib
 
-def ppsd(*args, **kwargs):
-    return main(*args, **kwargs)
+# Module name based on the file name
+_module_name = Path(__file__).parent.name
 
-__all__ = ["ppsd"]
+# Import the function in main.py with the same name as the module
+_main_module = importlib.import_module(".main", package=__package__)
+_main_func = getattr(_main_module, _module_name)
+
+# Define wrapper
+def _create_wrapper(func):
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
+
+globals()[_module_name] = _create_wrapper(_main_func)
+__all__ = [_module_name]
+del Path, importlib, _module_name, _main_module, _main_func, _create_wrapper
