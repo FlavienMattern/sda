d4d3b1a99551dc88cc4a86c0fdcd0492722862c9
FlavienMattern <f.mattern@unistra.fr>
2025-11-01 23:27:10 +0100

    Remove unused imports

--- sda/__init__.py
+++ sda/__init__.py
@@ -1,26 +1,2 @@
-# Dataset initialisation
-from .download_data import download_data
-from .scan_data import scan_data
-
-# PPSD computation
-from .ppsd import ppsd
-
-# Noise correlation methods
-from .xcorr_noise import xcorr_noise
-from .xcorr_noise2 import xcorr_noise2
-from .xcorr_noise_postprocessing2 import xcorr_noise_postprocessing2
-from .xcorr_noise_monitoring import xcorr_noise_monitoring
-
-
-# Events correlation methods
-from .xcorr_events import xcorr_events
-from .xcorr_events_monitoring import xcorr_events_monitoring
-from .xcorr_events_postprocessing import xcorr_events_postprocessing
-from .create_events_catalog import create_events_catalog
-
-# Various functions
-from . import functions
-
-# New imports for process folder
 from . import process
 __all__ = ["process"]
