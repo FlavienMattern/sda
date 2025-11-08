5832d2d490c9f841670db1b602e01a1813fa591d
FlavienMattern <f.mattern@unistra.fr>
2025-11-08 17:32:22 +0100

    Optimize SVD

--- sda/process/xcorr_noise_postprocessing/CorrelationsPostProcessing.py
+++ sda/process/xcorr_noise_postprocessing/CorrelationsPostProcessing.py
@@ -15,31 +15,26 @@ def CorrFilter(time, lagtime, data, config):
     K = config["WienerFiltTime"] # Wiener window in date dimension (smoothing of K days)
     L = config["WienerFiltLagTime"] # Wiener window in lag time dimension (1/L * fs = lag time smoothing)
     C = data
-    
-    
     idxGood = ~np.all(np.isnan(C), axis=0)
-    C_nonan = C[:,idxGood]
-    Cfull = np.zeros(np.shape(C_nonan))
-    
-    for j in range(1):
-        U, Sval, Vt = np.linalg.svd(C_nonan, full_matrices=True)
-        
-        for i in range(threshold):
-            # SVD Decomposition for singular value i
-            S = np.zeros(np.shape(Cfull))
-            S[i, i] = Sval[i]
-            C = U.dot(S).dot(Vt)
-            
-            # SVD Wiener
-            C = scipy.signal.wiener(C, (L, K))
-            
-            Cfull += C
-            
-        C_nonan = Cfull.copy()
-                
+    C_nonan = C[:, idxGood]
+
+    # SVD
+    U, Sval, Vt = np.linalg.svd(C_nonan, full_matrices=False)
+    U = U[:, :threshold]
+    Sval = Sval[:threshold]
+    Vt = Vt[:threshold, :]
+
+    # Wiener filter
+    Cfull = np.zeros_like(C_nonan)
+    for i in range(threshold):
+        Ci = np.outer(U[:, i], Vt[i, :]) * Sval[i]
+        Ci = scipy.signal.wiener(Ci, (L, K))
+        Cfull += Ci
+
     Cfull = scipy.signal.wiener(Cfull, (L, K))
-    
-    dataFilt = np.zeros( (len(lagtime), len(time)) ) * np.nan
-    dataFilt[:,idxGood] = Cfull
+
+    # Reconstruct matrix
+    dataFilt = np.full((len(lagtime), len(time)), np.nan)
+    dataFilt[:, idxGood] = Cfull
     
     return dataFilt
