bb6960be6131c6c446771de6acc7017a22d8f808
FlavienMattern <f.mattern@unistra.fr>
2025-11-08 01:19:47 +0100

    Correct input paths and add arguments

--- sda/core/xcorr_noise_monitoring.py
+++ sda/core/xcorr_noise_monitoring.py
@@ -6,33 +6,43 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-def average_dvv(output_path, comps=["ZZ","RR","TT","RT","RZ","TR","TZ","ZT","ZR"]):
+def average_dvv(output_path, comps=["ZZ","RR","TT","RT","RZ","TR","TZ","ZT","ZR"], methods=None, frequencies=None, stacks=None):
 
     monitoring_folder = os.path.join(output_path, "xcorr_noise_monitoring")
-    methods = os.listdir(monitoring_folder)
+    if methods is None:
+        list_methods = os.listdir(monitoring_folder)
+    else:
+        list_methods = methods.copy()
 
-    for method in methods:
+    for method in list_methods:
 
         method_folder = os.path.join(monitoring_folder, method)
-        freqs = os.listdir(method_folder)
-        for freq in freqs:
+        if frequencies is None:
+            list_freqs = os.listdir(method_folder)
+        else:
+            list_freqs = [f"{fmin:.2f}-{fmax:.2f}Hz" for fmin, fmax in frequencies]
+
+        for freq in list_freqs:
 
             freq_folder = os.path.join(method_folder, freq)
-            stacks = os.listdir(freq_folder)
+            if stacks is None:
+                list_stacks = os.listdir(freq_folder)
+            else:
+                list_stacks = [f"{int(s):03d}days" for s in stacks]
 
-            for stack in stacks:
+            for stack in list_stacks:
 
-                save_folder = os.path.join(stack, "avg")
-                files = os.listdir(os.path.join(save_folder, "ZZ"))
+                data_folder = os.path.join(freq_folder, stack)
+                files = os.listdir(os.path.join(data_folder, "ZZ"))
 
-                for file in tqdm(files, desc=f"Averaging dv/v [{method}, {freq}, {stack}]"):
+                for file in tqdm(files, desc=f"Averaging dv/v [{method}, {freq}, {stack}]", bar_format="{l_bar}{bar:30}{r_bar}"):
                     
                     sta1, sta2 = file.split(".")[0].split("-")
                     components = []
                     
                     for comp in comps:
                     # comps : ["ZZ","NN","EE","ZN","ZE","EZ","NZ","EN","NE"]:
-                        filename = os.path.join(save_folder, comp, f"{sta1}-{sta2}.csv")
+                        filename = os.path.join(data_folder, comp, f"{sta1}-{sta2}.csv")
                         if not os.path.exists(filename): continue 
                         datafile = pd.read_csv(filename, index_col=[0], header=[0,1])
                         datafile = datafile.set_index(pd.to_datetime(datafile.index, format="%Y-%m-%d"))
@@ -65,6 +75,7 @@ def average_dvv(output_path, comps=["ZZ","RR","TT","RT","RZ","TR","TZ","ZT","ZR"
                                 vals = stack_components[col].unstack(0)
                                 result[col] = vals.mean(axis=1)
                             
+                    save_folder = os.path.join(freq_folder, stack, "avg")
                     os.makedirs(save_folder, exist_ok=True)
                     save_filename = os.path.join(save_folder, f"{sta1}-{sta2}.csv")
                     result.to_csv(save_filename)
