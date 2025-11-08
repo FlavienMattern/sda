c3635a4cfa5231cc8fb3d8d142488ca3c8457c98
FlavienMattern <f.mattern@unistra.fr>
2025-11-08 18:56:58 +0100

    Handle postprocess xcorr files

--- sda/core/xcorr_noise.py
+++ sda/core/xcorr_noise.py
@@ -1,6 +1,7 @@
 import h5py
 import numpy as np
 from datetime import datetime
+import pandas as pd
 
 def load_h5_corr(file_path, time=None):
     """Read hdf5 file containing correlation functions for a given station pair.
@@ -18,20 +19,41 @@ def load_h5_corr(file_path, time=None):
 
     with h5py.File(file_path, "r") as f:
 
-        metadata_grp = f["metadata"]
-        fs = metadata_grp["fs"][()]
-        max_lag = metadata_grp["max_lag"][()]
-        corr_grp = f["correlations"]
-        lagtime = np.arange(-max_lag, max_lag + 1/fs, 1/fs)
+        if "metadata" in f.keys():
+            metadata_grp = f["metadata"]
+            fs = metadata_grp["fs"][()]
+            max_lag = metadata_grp["max_lag"][()]
+            corr_grp = f["correlations"]
+            lagtime = np.arange(-max_lag, max_lag + 1/fs, 1/fs)
+            
+            if time is None:
+                days = sorted(corr_grp.keys())
+                corr_list = [corr_grp[day][:] for day in days]
+                xcorr = np.stack(corr_list, axis=0)
+                times = np.array([datetime.strptime(day, "%Y-%m-%d") for day in days])
         
-        if time is None:
-            days = sorted(corr_grp.keys())
-            corr_list = [corr_grp[day][:] for day in days]
-            xcorr = np.stack(corr_list, axis=0)
-            times = np.array([datetime.strptime(day, "%Y-%m-%d") for day in days])
-       
+                full_times = pd.date_range(start=min(times), end=max(times), freq="D")
+                df = pd.DataFrame(xcorr, index=times, columns=lagtime)
+                df = df.reindex(index=full_times)
+                xcorr = df.values.T
+                times = df.index.to_pydatetime()
+
+            else:
+                xcorr = corr_grp[time][:]
+                times = datetime.strptime(time, "%Y-%m-%d")
+        
         else:
-            xcorr = corr_grp[time][:]
-            times = datetime.strptime(time, "%Y-%m-%d")
+            xcorr = f["array"][()]
+            lagtime = f["lagtime"][()]
+            times = f["time"][()]
+            fs = f["fs"][()]
+            times = times.astype(str)
+            times = np.array(times, dtype='datetime64[s]')
+
+            full_times = pd.date_range(start=min(times), end=max(times), freq="D")
+            df = pd.DataFrame(xcorr.T, index=times, columns=lagtime)
+            df = df.reindex(index=full_times)
+            xcorr = df.values.T
+            times = df.index.to_pydatetime()
             
     return xcorr, lagtime, times, fs
