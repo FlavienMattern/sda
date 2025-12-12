34b7fa1640839b87fc2b2e76ed90d03c9896f357
FlavienMattern <f.mattern@unistra.fr>
2025-12-12 15:54:51 +0100

    Update averaging with hdf5 format

--- sda/core/xcorr_noise_monitoring.py
+++ sda/core/xcorr_noise_monitoring.py
@@ -3,6 +3,7 @@ import pandas as pd
 from tqdm import tqdm
 import h5py
 import numpy as np
+from sda.process.xcorr_noise_monitoring.main import merge_hdf5
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -150,47 +151,39 @@ class dvv_object:
 
 
 
-def average_dvv(output_path, comps=["ZZ","RR","TT","RT","RZ","TR","TZ","ZT","ZR"], methods=None, frequencies=None, stacks=None):
+def average_dvv(output_path, comps=["ZZ","NN","EE","ZN","ZE","EZ","NZ","EN","NE"]):
 
     monitoring_folder = os.path.join(output_path, "xcorr_noise_monitoring")
-    if methods is None:
-        list_methods = os.listdir(monitoring_folder)
-    else:
-        list_methods = methods.copy()
+    files = os.listdir(monitoring_folder)
+    
+    for f in tqdm(files, desc=f"Averaging dv/v", bar_format="{l_bar}{bar:30}{r_bar}"):
+        
+        sta1, sta2 = f.split(".")[0].split("-")
+        filename = os.path.join(output_path, "xcorr_noise_monitoring", f"{sta1}-{sta2}.h5")
+        if not os.path.exists(filename): continue
+        dvvobj = load_h5_dvv(filename)
+        dvv_params = dvvobj.get_alldata()
+        results = {"metadata":{"sta1":sta1,"sta2":sta2}, "data":{}}
+        
 
-    for method in list_methods:
+        for method in dvv_params.keys():
+            results["data"][method] = {}
 
-        method_folder = os.path.join(monitoring_folder, method)
-        if frequencies is None:
-            list_freqs = os.listdir(method_folder)
-        else:
-            list_freqs = [f"{fmin:.2f}-{fmax:.2f}Hz" for fmin, fmax in frequencies]
+            for freq in dvv_params[method].keys():
+                results["data"][method][freq] = {}
 
-        for freq in list_freqs:
-
-            freq_folder = os.path.join(method_folder, freq)
-            if stacks is None:
-                list_stacks = os.listdir(freq_folder)
-            else:
-                list_stacks = [f"{int(s):03d}days" for s in stacks]
-
-            for stack in list_stacks:
-
-                data_folder = os.path.join(freq_folder, stack)
-                files = os.listdir(os.path.join(data_folder, "ZZ"))
-
-                for file in tqdm(files, desc=f"Averaging dv/v [{method}, {freq}, {stack}]", bar_format="{l_bar}{bar:30}{r_bar}"):
-                    
-                    sta1, sta2 = file.split(".")[0].split("-")
+                for stack in dvv_params[method][freq].keys():
+                    results["data"][method][freq][stack] = {}
+                        
                     components = []
                     
                     for comp in comps:
-                    # comps : ["ZZ","NN","EE","ZN","ZE","EZ","NZ","EN","NE"]:
-                        filename = os.path.join(data_folder, comp, f"{sta1}-{sta2}.csv")
-                        if not os.path.exists(filename): continue 
-                        datafile = pd.read_csv(filename, index_col=[0], header=[0,1])
-                        datafile = datafile.set_index(pd.to_datetime(datafile.index, format="%Y-%m-%d"))
-                        components.append(datafile)
+                           
+                        try:
+                            df = dvvobj.get_data(method, freq, stack, comp)
+                        except:
+                            continue
+                        components.append(df)
                         
                     if len(components) == 0:
                         continue
@@ -205,9 +198,9 @@ def average_dvv(output_path, comps=["ZZ","RR","TT","RT","RZ","TR","TZ","ZT","ZR"
                         for col in components[0].columns:
                             level_name = col[-1]
                             
-                            if level_name == "dv":
+                            if level_name == "dvv":
                                 prefix = col[:-1]
-                                cc_col = prefix + ("cc",)
+                                cc_col = prefix + ("coherence",)
                                 
                                 dv_vals = stack_components[col].unstack(0)
                                 cc_vals = stack_components[cc_col].unstack(0)
@@ -219,7 +212,20 @@ def average_dvv(output_path, comps=["ZZ","RR","TT","RT","RZ","TR","TZ","ZT","ZR"
                                 vals = stack_components[col].unstack(0)
                                 result[col] = vals.mean(axis=1)
                             
-                    save_folder = os.path.join(freq_folder, stack, "avg")
-                    os.makedirs(save_folder, exist_ok=True)
-                    save_filename = os.path.join(save_folder, f"{sta1}-{sta2}.csv")
-                    result.to_csv(save_filename)
+                    # save_folder = os.path.join(freq_folder, stack, "avg")
+                    # os.makedirs(save_folder, exist_ok=True)
+                    # save_filename = os.path.join(save_folder, f"{sta1}-{sta2}.csv")
+                    # result.to_csv(save_filename)
+                    results["data"][method][freq][stack]["avg"] = {}
+                    lags = result.columns.get_level_values(0).unique().tolist()
+                    
+                    for lag_str in lags:
+                        results["data"][method][freq][stack]["avg"][lag_str] = {
+                            "time": result.index.strftime("%Y-%m-%d").tolist(),
+                            "dvv": result[lag_str]["dvv"].tolist(),
+                            "coherence": result[lag_str]["coherence"].tolist(),
+                            "error": result[lag_str]["error"].tolist(),
+                        }                  
+
+        with h5py.File(filename, "a") as f:
+            merge_hdf5(f, results)
